0edad2a4ce394f7cd44f16995428ee7af0a8e95c
sweet-aludra <adryps71@gmail.com>
2023-12-15 19:11:47 -0300

    ex1

--- 01.py
+++ 01.py
@@ -1,2 +1,8 @@
 #1.Escreva um programa que solicite dois números ao usuário e exiba a soma deles.
 
+num1 = int(input("digite o primeiro numero: "))
+num2 = int(input("digite um segundo numero: "))
+
+soma = num1 + num2
+
+print(f"a soma do primeiro numero {num1} com o segundo numero {num2} e igual a: {soma}")
