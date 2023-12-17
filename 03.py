5ebb61a4cf1f78083c33aa3922012ddfaedd85b6
sweet-aludra <adryps71@gmail.com>
2023-12-17 15:32:09 -0300

    att

---
+++ 03.py
@@ -0,0 +1,7 @@
+#3.Desenvolva um programa que peça o raio de um círculo e calcule a área.
+
+raio = int(input("digite o raio do circulo: "))
+
+area = 3.14 * raio ** 2
+
+print(f"a area do circulo e: {area}")
---
+++ 04.py
@@ -0,0 +1,6 @@
+#4.Faça um programa que converta uma temperatura de Celsius para Fahrenheit ou vice-versa, dependendo da escolha do usuário.
+tCelsius = int(input("digite a temperatura: "))
+
+tFahrenheit = (tCelsius * 9/5) + 32
+
+print(f"temperatura em Fahrenheit: {tFahrenheit}")
